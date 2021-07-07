438f10c9694b40c1969f73d73ece80cba2c66672
unknown <vothanhtri100898@gmail.com>
2021-07-07 20:12:18 +0700

    Done 7/2021

---
+++ README.md
@@ -0,0 +1,11 @@
+Họ tên: Võ Thanh Trí
+
+* Phân tích dự án TheBand
+1. Header
+2. Slider
+3. Content
+    1. About
+    2. Tour
+    3. Contact
+    4. Image
+4. Footer
---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+let isHidden = true;
+const headerElement = document.getElementById('header');
+const iconMenuElement = document.getElementById('search-btn');
+const headerHeight = headerElement.clientHeight;
+
+// Đóng/ mở mobile menu
+iconMenuElement.onclick = () => {
+    let isClose = headerElement.clientHeight == headerHeight;
+    if (isClose)
+        headerElement.style.height = 'auto';
+    else
+        headerElement.style.height = null;
+}
+
+// Ẩn menu khi nhấn vào item
+const menuItemElements = document.querySelectorAll('.hide-on-click');
+for (let i = 0; i < menuItemElements.length; i++) {
+    menuItemElements[i].onclick = function (e) {
+        let isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if (isParentMenu) {
+            if (window.innerWidth < 740) {
+                let subnav = document.querySelector('.subnav');
+                if (isHidden) {
+                    subnav.style.display = 'block';
+                    isHidden = false;
+                }
+                else {
+                    subnav.style.display = 'none';
+                    isHidden = true;
+                }
+            }
+            e.preventDefault();
+        } else {
+            headerElement.style.height = null;
+        }
+    }
+}
